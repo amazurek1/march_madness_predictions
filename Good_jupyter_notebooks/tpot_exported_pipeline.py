98906691db506bbd3c8a2465447d485a8072f30b
Aviva Mazurek <aviva.mazurek@stonybrook.edu>
2020-03-02 15:42:07 -0500

    a ton of data analysis!

--- Good_jupyter_notebooks/tpot_exported_pipeline.py
+++ Good_jupyter_notebooks/tpot_exported_pipeline.py
@@ -1,11 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.feature_selection import SelectFwe, f_classif
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import PolynomialFeatures
 from tpot.builtins import StackingEstimator
-from xgboost import XGBClassifier
 from tpot.export_utils import set_param_recursive
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
@@ -14,11 +13,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.7377438102061937
+# Average CV score on the training set was: 0.7427991401958443
 exported_pipeline = make_pipeline(
-    SelectFwe(score_func=f_classif, alpha=0.043000000000000003),
-    StackingEstimator(estimator=XGBClassifier(learning_rate=0.001, max_depth=2, min_child_weight=17, n_estimators=100, nthread=1, subsample=0.4)),
-    LogisticRegression(C=0.1, dual=False, penalty="l2")
+    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
+    StackingEstimator(estimator=RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.25, min_samples_leaf=9, min_samples_split=20, n_estimators=100)),
+    GradientBoostingClassifier(learning_rate=0.01, max_depth=10, max_features=0.7500000000000001, min_samples_leaf=12, min_samples_split=18, n_estimators=100, subsample=0.2)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)

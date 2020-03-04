8974bc319fb584ee9597d5e19d4f79c8ca455c55
Aviva Mazurek <aviva.mazurek@stonybrook.edu>
2020-03-04 11:55:02 -0500

    data analysis

--- Good_jupyter_notebooks/tpot_exported_pipeline.py
+++ Good_jupyter_notebooks/tpot_exported_pipeline.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline, make_union
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import StandardScaler
 from tpot.builtins import StackingEstimator
 from tpot.export_utils import set_param_recursive
 
@@ -13,11 +13,11 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=42)
 
-# Average CV score on the training set was: 0.7427991401958443
+# Average CV score on the training set was: 0.8878221534432473
 exported_pipeline = make_pipeline(
-    PolynomialFeatures(degree=2, include_bias=False, interaction_only=False),
-    StackingEstimator(estimator=RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.25, min_samples_leaf=9, min_samples_split=20, n_estimators=100)),
-    GradientBoostingClassifier(learning_rate=0.01, max_depth=10, max_features=0.7500000000000001, min_samples_leaf=12, min_samples_split=18, n_estimators=100, subsample=0.2)
+    StandardScaler(),
+    StackingEstimator(estimator=LogisticRegression(C=0.1, dual=False, penalty="l2")),
+    SGDClassifier(alpha=0.01, eta0=1.0, fit_intercept=True, l1_ratio=1.0, learning_rate="invscaling", loss="hinge", penalty="elasticnet", power_t=0.0)
 )
 # Fix random state for all the steps in exported pipeline
 set_param_recursive(exported_pipeline.steps, 'random_state', 42)
